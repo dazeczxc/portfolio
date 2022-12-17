f61b2b186442ead1494d5ea92fbf6d4e86f2d23c
dazec <blackdazec@gmail.com>
2022-12-17 22:01:57 +0800

    add opne and close button for overlays in small device

--- js/main.js
+++ js/main.js
@@ -41,5 +41,65 @@ window.addEventListener("load", function(){
   loader.style.display= "none";
 });
 
+ 
+
+
+if (window.innerWidth < 769) {
+  //1
+  document.getElementById("close-button1").addEventListener("click", function() {
+    document.getElementById("overlay1").style.display = "none";
+    document.getElementById("title11").style.opacity = "1";
+    document.getElementById("title22").style.opacity = "1";
+  });
+  
+  document.getElementById("open-button1").addEventListener("click", function() {
+    document.getElementById("overlay1").style.display = "flex";
+    document.getElementById("title11").style.opacity = "0";
+    document.getElementById("title12").style.opacity = "0";
+  });
+
+//2
+  document.getElementById("close-button2").addEventListener("click", function() {
+    document.getElementById("overlay2").style.display = "none";
+    document.getElementById("title21").style.opacity = "1";
+    document.getElementById("title22").style.opacity = "1";
+  });
+  
+  document.getElementById("open-button2").addEventListener("click", function() {
+    document.getElementById("overlay2").style.display = "flex";
+    document.getElementById("title21").style.opacity = "0";
+    document.getElementById("title22").style.opacity = "0";
+  });
+
+
+//3
+  document.getElementById("close-button3").addEventListener("click", function() {
+    document.getElementById("overlay3").style.display = "none";
+    document.getElementById("title31").style.opacity = "1";
+    document.getElementById("title32").style.opacity = "1";
+  });
+  
+  document.getElementById("open-button3").addEventListener("click", function() {
+    document.getElementById("overlay3").style.display = "flex";
+    document.getElementById("title31").style.opacity = "0";
+    document.getElementById("title32").style.opacity = "0";
+  });
+
+  //4
+  document.getElementById("close-button4").addEventListener("click", function() {
+    document.getElementById("overlay4").style.display = "none";
+    document.getElementById("title41").style.opacity = "1";
+    document.getElementById("title42").style.opacity = "1";
+  });
+  
+  document.getElementById("open-button4").addEventListener("click", function() {
+    document.getElementById("overlay4").style.display = "flex";
+    document.getElementById("title41").style.opacity = "0";
+    document.getElementById("title42").style.opacity = "0";
+  });
+}
+
+
+
 
 
