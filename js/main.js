140dd4778ec7ae39887466094e7b6291f070a9f5
dazec <blackdazec@gmail.com>
2022-12-30 12:01:39 +0800

    disappearing navbar

--- js/main.js
+++ js/main.js
@@ -4,36 +4,7 @@ document.getElementById('links').onclick = function () {
 };
 
 
-//custom scroll reveal about section
-//myID = document.getElementById("about");
 
-//var myScrollFunc = function () {
-// var y = window.scrollY;
-// if (y >= 50) {
-//   myID.className = "about show";
-// } else {
-//   myID.className = "about hide";
-// }
-//};
-//window.addEventListener("scroll", myScrollFunc);
-//
-
-//navbar hides on scroll down, shows on scroll up
-//var lastScrollTop = 0;
-//navbar = document.getElementById('navbarr');
-
-//window.addEventListener("scroll", function () {
-
-//  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
- // if (scrollTop > lastScrollTop) {
- //   navbar.style.top = "-80px";
- // } else {
- //   navbar.style.top = "0";
- // }
-
- // lastScrollTop = scrollTop;
-//});
 
 
 var loader = document.getElementById("preloader");
@@ -100,6 +71,24 @@ if (window.innerWidth < 769) {
 }
 
 
+ 
 
+
+//navbar hides on scroll down, shows on scroll up
+var lastScrollTop = 0;
+navbar = document.getElementById('navbarr');
+
+window.addEventListener("scroll", function () {
+
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-80px";
+  } else {
+    navbar.style.top = "0";
+  }
+
+  lastScrollTop = scrollTop;
+});
 
 
